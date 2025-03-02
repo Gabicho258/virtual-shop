5a96a45100a92d30774570bdcf9afc9be1f0e878
Gabriel Antony <gmamanican@unsa.edu.pe>
2025-03-02 09:50:03 -0500

    add product pagination functionality; implement pagination logic and create Pagination component for product listing

---
+++ src/actions/product/product-pagination.ts
@@ -0,0 +1,44 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+
+interface PaginationOptions {
+  page?: number;
+  take?: number;
+}
+
+export const getPaginatedProductsWithImages = async ({
+  page = 1,
+  take = 12,
+}: PaginationOptions) => {
+  if (isNaN(Number(page))) page = 1;
+  if (page < 1) page = 1;
+
+  try {
+    const products = await prisma.product.findMany({
+      take: take,
+      skip: (page - 1) * take,
+      include: {
+        ProductImage: {
+          take: 2,
+          select: {
+            url: true,
+          },
+        },
+      },
+    });
+
+    const totalPages = Math.ceil((await prisma.product.count()) / take);
+
+    return {
+      currentPage: page,
+      totalPages: totalPages,
+      products: products.map((product) => ({
+        ...product,
+        images: product.ProductImage.map((image) => image.url),
+      })),
+    };
+  } catch (error) {
+    throw new Error("Error fetching products" + error);
+  }
+};
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,13 +1,32 @@
-import { ProductGrid, Title } from "@/components";
-import { initialData } from "@/seed/seed";
+import { getPaginatedProductsWithImages } from "@/actions";
+import { Pagination, ProductGrid, Title } from "@/components";
+import { redirect } from "next/navigation";
+// import { initialData } from "@/seed/seed";
 
-const products = initialData.products;
+// const products = initialData.products;
 
-export default function Home() {
+interface Props {
+  searchParams: {
+    page?: string;
+  };
+}
+
+export default async function Home({ searchParams }: Props) {
+  const { page: searchParamsPage } = await searchParams;
+
+  const page = searchParamsPage ? parseInt(searchParamsPage) : 1;
+
+  const { products, currentPage, totalPages } =
+    await getPaginatedProductsWithImages({ page });
+
+  console.log({ currentPage, totalPages });
+  if (products.length === 0) redirect("/");
+
   return (
     <>
       <Title title="Shop" subtitle="All products" className="mb-2" />
       <ProductGrid products={products} />
+      <Pagination totalPages={5} />
     </>
   );
 }
