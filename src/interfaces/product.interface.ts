730ae367df718b52e8a9b3e985453f2c5a377a32
Gabriel Antony <gmamanican@unsa.edu.pe>
2025-03-26 13:54:28 -0500

    feat: add category and product image interfaces; create CategoryProduct interface and extend ProductImage interface for better product management

--- src/interfaces/product.interface.ts
+++ src/interfaces/product.interface.ts
@@ -25,3 +25,9 @@ export interface CartProduct {
 export type Size = "XS" | "S" | "M" | "L" | "XL" | "XXL" | "XXXL";
 export type Type = "shirts" | "pants" | "hoodies" | "hats";
 export type Category = "men" | "women" | "kid" | "unisex";
+
+export interface ProductImage {
+  id: string;
+  url: string;
+  productId: string;
+}
