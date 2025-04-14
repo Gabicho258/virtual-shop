e2d68628c7aa48dade48a1d24e811fa71ce10e29
Gabriel Antony <gmamanican@unsa.edu.pe>
2025-04-14 00:02:41 -0500

    feat: refactor createUpdateProduct action; enhance product handling and transaction logic

--- src/actions/product/create-update-product.ts
+++ src/actions/product/create-update-product.ts
@@ -1,6 +1,7 @@
 "use server";
 
-import { Gender } from "@prisma/client";
+import prisma from "@/lib/prisma";
+import { Gender, Product, Size } from "@prisma/client";
 import { z } from "zod";
 
 const productSchema = z.object({
@@ -29,6 +30,46 @@ export const createUpdateProduct = async (formData: FormData) => {
     console.error("Validation errors:", parsedData.error.format());
     return { ok: false };
   }
-  console.log(parsedData.data);
+  const product = parsedData.data;
+  const { id, ...rest } = product;
+  product.slug = product.slug.toLowerCase().replace(/ /g, "-").trim();
+
+  const prismaTx = await prisma.$transaction(async (tx) => {
+    let product: Product;
+
+    const tagsArray = rest.tags
+      .split(",")
+      .map((tag) => tag.trim().toLowerCase());
+
+    if (id) {
+      product = await tx.product.update({
+        where: { id },
+        data: {
+          ...rest,
+          sizes: {
+            set: rest.sizes as Size[],
+          },
+          tags: {
+            set: tagsArray,
+          },
+        },
+      });
+    } else {
+      product = await tx.product.create({
+        data: {
+          ...rest,
+          sizes: {
+            set: rest.sizes as Size[],
+          },
+          tags: {
+            set: tagsArray,
+          },
+        },
+      });
+    }
+    console.log({ product });
+    return product;
+  });
+
   return { ok: true };
 };
